92c8a5c55d46239a874781ac0fc4f38600814fde
TonyCHS <92799152+TonyCHS@users.noreply.github.com>
2022-03-03 11:17:55 +0800

    This is Blog App v2

--- App.js
+++ App.js
@@ -1,20 +1,39 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import LoggedInTabStack from "./components/LoggedInTabStack";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import SignInSignUpScreen from "./screens/SignInSignUpScreen";
+import { Provider, useSelector } from "react-redux";
+import store from "./redux/configureStore";
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function App() {
+
+  const token = useSelector((state) => state.auth.token);
+  const isDark = useSelector((state) => state.accountPrefs.isDark);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar style={isDark ? "light" : "dark"}/>
+        <Stack.Navigator
+          mode="modal"
+          headerMode="none"
+          initialRouteName={token != null ? "Logged In" : "SignInSignUp"}
+          animationEnabled={false}>
+        <Stack.Screen component={SignInSignUpScreen} name="SignInSignUp" />
+        <Stack.Screen component={LoggedInTabStack} name="Logged In" />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default function AppWrapper() {
+  return (
+    <Provider store={store}>
+      <App/>
+    </Provider>
+  )
+}
+
